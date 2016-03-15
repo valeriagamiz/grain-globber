4a7b2d340e0271fc3f593e1222cb646976bed0dc
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-03-15 12:56:42 -0400

    again

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-var $ul = $('ul');
+-  var $ul = $('ul');
+ +var $ul = $('ul');
 
-grains.forEach(function (grain) {
-  var $name = $('<h2>');
-  var $img = $('<img>');
-  var $desc = $('<p>');
-  var $li = $('<li>');
+ -  grains.forEach (function(grain){
+ +grains.forEach(function (grain) {
+    var $name = $('<h2>');
+    var $img = $('<img>');
+    var $desc = $('<p>');
+    var $li = $('<li>');
 
-  $img.attr('src', 'images/' + grain.img);
-  $desc.html(grain.desc);
-  $name.html(grain.name);
+    $img.attr('src', 'images/' + grain.img);
+ -  $name.html(grain.name);
+    $desc.html(grain.desc);
+ +  $name.html(grain.name);
 
-  $li.append($img);
-  $li.append($name);
-  $li.append($desc);
-  $ul.append($li);
-});
+    $li.append($img);
+    $li.append($name);
+    $li.append($desc);
+    $ul.append($li);
+ -  });
+ +});
